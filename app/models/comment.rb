21931d3c7d43d947a07d8d0619512f7507f51971
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2014-10-01 21:57:42 +0200

    Repair empty comment bug.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :story
   belongs_to :user
 
+  validates :extended, length: { minimum: 2 }, presence: true
+
   def self.recent
     order("created_at DESC")
   end
